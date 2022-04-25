f77c84ae908c863a8460a11527a5f4f7d48a71c4
Mockingbird <chenchuw@bu.edu>
2022-04-25 17:08:27 -0400

    update script of voice to text

---
+++ Voice Recognition/voice2text.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "*** Recording now, please speak ***"
+arecord --format=S16_LE --duration=5 --rate=16000 --file-type=wav test.wav
+
+echo "*** Processing... ***"
+curl -X POST --data-binary @test.wav --header 'Content-Type: audio/l16; rate=16000;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyAGpTYKWIWyIVYsIAqn_1MZ8QRNKrBLlKQ' > converted.txt
+
+echo "*** Display converted text ***"
+cat converted.txt
+
+rm test.wav
+# rm converted.txt
